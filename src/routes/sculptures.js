c5446f33be7501e14a0544c11e2ad4e6dc55413e
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-07-12 23:30:04 +0200

    dt

--- src/routes/sculptures.js
+++ src/routes/sculptures.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const sculpturesController = require('../controllers/sculptures');
+const {
+    getAllSculptures,
+    getSculptureById,
+    createSculpture,
+    updateSculptureById,
+    deleteSculpture,
+  } = require('../controllers/sculptures');
 
-router.get('/sculptures', sculpturesController.getAllSculptures);
-router.get('/sculptures/:id', sculpturesController.getSculptureById);
-router.post('/sculptures', sculpturesController.createSculpture);
-router.put('/sculptures/:id', sculpturesController.updateSculptureById);
-router.delete('/sculptures/:id', sculpturesController.deleteSculpture);
+router.get('/sculptures', getAllSculptures);
+router.get('/sculptures/:id', getSculptureById);
+router.post('/sculptures', createSculpture);
+router.put('/sculptures/:id', updateSculptureById);
+router.delete('/sculptures/:id', deleteSculpture);
 
 module.exports = router;
