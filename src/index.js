ec6efe146bd2a8cd766ba38064f3f927b6c027bb
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-07-12 18:56:11 +0200

    routes + mock controllers

--- src/index.js
+++ src/index.js
@@ -3,15 +3,48 @@ const express = require('express');
 const app = express();
 const {connectionDB} = require ('./config/db.js')
 
+const router = express.Router();
+
 app.use(express.json())
 
-const router = express.Router()
+
+const getAllSculptures = async (req, res, next) => {
+    
+  res.status(200).send("getallSculptures")
+};
+
+
+const getSculptureById = async (req, res, next) => {
+  res.status(200).send("getSculptureById")
+};
+
+
+const createSculpture = async (req, res, next) => {
+  res.status(200).send("createSculpture")
+};
+
+
+
+const updateSculptureById = async (req, res, next) => {
+  res.status(200).send("updateSculptureById")
+};
+
+const deleteSculpture = async (req, res, next) => {
+  res.status(200).send("deleteSculpture")
+};
+
+router.get('/sculptures', getAllSculptures)
+router.get('/sculptures/:id', getSculptureById)
+router.post('/sculptures', createSculpture)
+router.put('/sculptures/:id', updateSculptureById)
+router.delete('/sculptures/:id', deleteSculpture)
+
 
 
 
 app.use('/api', router)
 
-router.use('/sculptures', (req,res,next)=>{res.send("sculptures")})
+
 
 app.use('*', (req, res, next) => {
   res.status(404).json({data: 'Error 404: Page not found'})
