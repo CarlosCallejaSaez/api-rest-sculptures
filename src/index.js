ab54d39580fb6a82db0d7f798ff07a956ec417c2
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-07-13 09:40:02 +0200

    checked

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const { connectionDB } = require('./config/db.js');
 
+
+
+
+
 const sculpturesRouter = require('./routes/sculptures');
 
 app.use(express.json());
 
 app.use('/api', sculpturesRouter);
 
+
+
 app.use('*', (req, res, next) => {
   res.status(404).json({ data: 'Error 404: Page not found' });
 });
+
 
 app.use((error, req, res, next) => {
   res.status(500).json({ data: 'Internal Server Error' });
